4c5575eec01dc9d4186d5468026e8806830ff71c
Robert090903 <amparorobert2@gmail.com>
2025-04-11 08:30:04 +0800

    add controllers and fix routes

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('categories', CategoryController::class);
+
+// Remove the duplicate import
+// use App\Http\Controllers\ProductController;
+
 Route::apiResource('products', ProductController::class);
